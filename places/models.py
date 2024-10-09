0b4f0d0e9834202cdf35703423c1cbece84f4ea5
Shishkin PRO <viktorshish@gmail.com>
2024-10-10 00:16:12 +0300

    Add model for images

--- places/models.py
+++ places/models.py
@@ -10,3 +10,12 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    image = models.ImageField('Фотография')
+    position = models.PositiveIntegerField('Позиция', unique=True, null=True)
+
+    def __str__(self):
+        return f'{self.position} {self.place}'
