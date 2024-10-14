734304d151d70ff99fcad05f58a653248d22bb97
Shishkin PRO <viktorshish@gmail.com>
2024-10-14 23:43:51 +0300

    Create API Endpoint, get place

--- places/views.py
+++ places/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.conf import settings
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 
 from places.models import Place
 
@@ -29,3 +31,8 @@ def index(request):
 
     context = {'places_geojson': places_geojson}
     return render(request, 'index.html', context)
+
+
+def get_place(request, place_id):
+    place = get_object_or_404(Place, pk=place_id)
+    return HttpResponse(place.title)
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-
 from django.urls import path
+
 from places import views
 
 
 urlpatterns = ([
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('places/<int:place_id>/', views.get_place)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
