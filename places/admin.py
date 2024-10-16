d956bb11eae42c17032a82ec99f0f9306e789d20
Shishkin PRO <viktorshish@gmail.com>
2024-10-16 22:16:43 +0300

    Adding photos in the Places admin

--- places/admin.py
+++ places/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 from places.models import Place, Image
 
 
-admin.site.register(Place)
-admin.site.register(Image)
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 1
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]
